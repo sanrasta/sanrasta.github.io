de39d153df5389748edf3ffd3b2ac117ebe7b193
Santiago Ramirez <ramirezfsantiago@gmail.com>
2023-01-24 16:43:27 -0600

    solar change

--- pages/components/Deck.jsx
+++ pages/components/Deck.jsx
@@ -13,7 +13,11 @@ function Deck(){
     <div className="flex flex-wrap -m-4">
       <div className="lg:w-1/3 sm:w-1/2 p-4">
         <div className="flex relative">
-          <Image alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src={'https://images.unsplash.com/photo-1623701675981-1b64ba3ecca2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'}/>
+          <Image 
+          alt="gallery" 
+          className="absolute inset-0 w-full h-full object-cover object-center" 
+          layout='fill'
+          src={'https://images.unsplash.com/photo-1623701675981-1b64ba3ecca2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80'}/>
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-90">
            {/* <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
             */}<h1 className="title-font text-lg font-medium text-gray-900 mb-3">Shooting Stars</h1>
@@ -24,7 +28,11 @@ to pay more next year than they are today.</p>
       </div>
       <div className="lg:w-1/3 sm:w-1/2 p-4">
         <div className="flex relative">
-          <Image alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src='https://images.unsplash.com/photo-1641349630154-1c1024379a88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1413&q=80' />
+          <Image 
+          alt="gallery" 
+          layout='fill'
+          className="absolute inset-0 w-full h-full object-cover object-center" 
+          src='https://images.unsplash.com/photo-1641349630154-1c1024379a88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1413&q=80' />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-90">
             <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The Catalyzer</h1>
@@ -34,7 +42,11 @@ to pay more next year than they are today.</p>
       </div>
       <div className="lg:w-1/3 sm:w-1/2 p-4">
         <div className="flex relative">
-          <Image alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://images.unsplash.com/photo-1578439297699-eb414262c2de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2020&q=80" />
+          <Image 
+          alt="gallery" 
+          layout='fill'
+          className="absolute inset-0 w-full h-full object-cover object-center" 
+          src="https://images.unsplash.com/photo-1578439297699-eb414262c2de?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2020&q=80" />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-100">
             <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">The 400 Blows</h1>
@@ -44,7 +56,11 @@ to pay more next year than they are today.</p>
       </div>
       <div className="lg:w-1/3 sm:w-1/2 p-4">
         <div className="flex relative">
-          <Image alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://images.unsplash.com/photo-1624397640148-949b1732bb0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80" />
+          <Image 
+            alt="gallery"
+            layout='fill'
+            className="absolute inset-0 w-full h-full object-cover object-center" 
+            src="https://images.unsplash.com/photo-1624397640148-949b1732bb0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80" />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-90">{/*
             <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>*/}
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Neptune</h1>
@@ -54,21 +70,29 @@ to pay more next year than they are today.</p>
       </div>
       <div className="lg:w-1/3 sm:w-1/2 p-4">
         <div className="flex relative">
-          <Image alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://images.unsplash.com/photo-1637100533381-d7e827eafd3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
-          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-90">{/*
-            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>*/}
+          <Image 
+            alt="gallery" 
+            layout='fill'
+            className="absolute inset-0 w-full h-full object-cover object-center" 
+            src="https://images.unsplash.com/photo-1637100533381-d7e827eafd3d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" />
+          <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-90">
+            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Holden Caulfield</h1>
-            <p className="leading-relaxed">Each KW you produce is a KW that you do not have to purchase from your electric company. Now think about all the KW you will produce in the future.</p>
+            <p className="leading-relaxed">Each KW you produce is a KW that you do not have to purchase from your electric company. </p>
           </div>
         </div>
       </div>
       <div className="lg:w-1/3 sm:w-1/2 p-4">
         <div className="flex relative">
-          <Image alt="gallery" className="absolute inset-0 w-full h-full object-cover object-center" src="https://images.unsplash.com/photo-1463173904305-ba479d2123b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2158&q=80" />
+          <Image 
+            alt="gallery" 
+            layout='fill'
+            className="absolute inset-0 w-full h-full object-cover object-center" 
+            src="https://images.unsplash.com/photo-1463173904305-ba479d2123b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2158&q=80" />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-90">
-            <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>
+           {/* <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>*/}
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Alper Kamu</h1>
-            <p className="leading-relaxed">Another amazing reason to go solar today - get the economic benefits NOW, and get more for the sale of your home TOMORROW.</p>
+            <p className="leading-relaxed">Another amazing reason to go solar in your home today is so that you can get the economic benefits NOW, and get more for the sale of your home TOMORROW.</p>
           </div>
         </div>
       </div>
