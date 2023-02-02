3e802d4ce4f5aef2fe2595b49fb2df91062b3d76
Santiago Ramirez <ramirezfsantiago@gmail.com>
2023-02-01 21:04:33 -0600

    solar change

--- pages/components/Deck.jsx
+++ pages/components/Deck.jsx
@@ -88,7 +88,7 @@ function Deck(){
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-90">
            {/* <h2 className="tracking-widest text-sm title-font font-medium text-indigo-500 mb-1">THE SUBTITLE</h2>*/}
             <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Incentives</h1>
-            <p className="leading-relaxed">Find out if you are elegible for a govermental program in order to get the help needed to equip your home with the ultimate technology in solar solutions.</p>
+            <p className="leading-relaxed">Find out if you are elegible for a governmental program in order to get the help needed to equip your home with the ultimate technology in solar solutions.</p>
           </div>
         </div>
       </div>
