01eeb48c0a191b218084c4b2d14c81b8f23d874c
Santiago Ramirez <ramirezfsantiago@gmail.com>
2023-02-01 19:44:05 -0600

    solar change

--- pages/components/Deck.jsx
+++ pages/components/Deck.jsx
@@ -33,8 +33,8 @@ function Deck(){
           className="absolute inset-0 w-full h-full object-cover object-center" 
           src='https://images.unsplash.com/photo-1641349630154-1c1024379a88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1413&q=80' />
           <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-200 bg-white opacity-0 hover:opacity-90">
-            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Sign Up</h1>
-            <p className="leading-relaxed">It is a very simple and breif process.<br></br> We collect your utility bill so we can design a custom savings proposal for your home specific needs.</p>
+            <h1 className="title-font text-lg font-medium text-gray-900 mb-3">Sign Up!</h1>
+            <p className="leading-relaxed">It is a very simple and brief process.<br></br> We collect your utility bill in order to design a custom savings proposal for your home specific needs.</p>
           </div>
         </div>
       </div>
